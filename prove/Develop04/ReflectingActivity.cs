c29c84a949de218ea65980f835fe60bd6ef8ab72
gospel <gospel4levi@gmail.com>
2024-05-25 19:55:27 +0100

    Commit assignment changes for week 4

--- prove/Develop04/ReflectingActivity.cs
+++ prove/Develop04/ReflectingActivity.cs
@@ -21,7 +21,7 @@ class ReflectingActivity : Activity
             "How did you get started?",
             "How did you feel when it was complete?",
             "What made this time different than other times when you were not as successful?",
-            "What is your favorite thing about this experience?",
+            "What is your favorite thing about this life experience?",
             "What could you learn from this experience that applies to other situations?",
             "What did you learn about yourself through this experience?",
             "How can you keep this experience in mind in the future?"
